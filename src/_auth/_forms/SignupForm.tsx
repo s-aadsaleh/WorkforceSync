139d20113076d407e9b660008232970b5fb9718a
s-aadsaleh <s.aadsaleh@icloud.com>
2023-12-11 00:08:05 +0530

    vercel deployment fixes
    fixed the unused imports that were causing vercel to not properly deploy

--- src/_auth/_forms/SignupForm.tsx
+++ src/_auth/_forms/SignupForm.tsx
@@ -1,36 +1,9 @@
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-
-import { buttonVariants } from "@/components/new-york/ui/button"
 import { UserAuthForm } from "@/components/user-auth-form"
 import { Link } from "react-router-dom"
-import { cn } from "@/lib/utils"
 
 
-
-
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
 
 const SignupForm = () => {
-
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
- 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
 
   return (
     <div >
